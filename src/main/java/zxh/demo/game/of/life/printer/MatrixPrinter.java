828016bd05f6f43bae2f4fd8117e308728c2abd6
zhangxuhai <xuhai.zhang@thoughtworks.com>
2019-11-06 23:46:19 +0800

    feat: provide chart utils to print matrix

--- src/main/java/zxh/demo/game/of/life/printer/MatrixPrinter.java
+++ src/main/java/zxh/demo/game/of/life/printer/MatrixPrinter.java
@@ -1,7 +1,13 @@
 package zxh.demo.game.of.life.printer;
 
+import org.knowm.xchart.SwingWrapper;
 import org.knowm.xchart.XYChart;
 import org.knowm.xchart.XYChartBuilder;
+import org.knowm.xchart.XYSeries;
+import org.knowm.xchart.style.markers.SeriesMarkers;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * MatrixPrinter:
@@ -13,18 +19,33 @@ public class MatrixPrinter {
                 .height(1024).width(1024)
                 .title("Matrix").xAxisTitle("").yAxisTitle("")
                 .build();
+    private static SwingWrapper sw = new SwingWrapper(chart);
+    static {
+        chart.getStyler().setDefaultSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Scatter);
+        chart.addSeries("Alive", new int[]{0}, new int[]{0});
+        sw.displayChart();
+    }
 
     public static void printByMatrix(int[][] intMatrix) {
         int xLen = intMatrix.length;
         int yLen = intMatrix[0].length;
-        int[] x = new int[xLen];
-        int[] y = new int[yLen];
+        List<int[]> resultList = new ArrayList<>();
         for (int i = 0; i < xLen; i++) {
             for (int j = 0; j < yLen; j++) {
                 if (intMatrix[i][j] != 0) {
-
+                    resultList.add(new int[]{i, j});
                 }
             }
         }
+
+        double[] x = new double[resultList.size()];
+        double[] y = new double[resultList.size()];
+        for (int i = 0; i < resultList.size(); i++) {
+            x[i] = resultList.get(i)[0];
+            y[i] = resultList.get(i)[1];
+        }
+
+        chart.updateXYSeries("Alive", x, y, null).setMarker(SeriesMarkers.CIRCLE);
+        sw.repaintChart();
     }
 }
